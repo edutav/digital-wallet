f9c8abbf38880378b9d313f01029f689f2e7a737
Eduardo Tavares <edutav@gmail.com>
2024-12-14 00:21:26 -0400

    Add JwtUtils class for JWT token validation and user extraction

---
+++ src/main/java/com/edutav/digitalwallet/infrastructure/security/JwtUtils.java
@@ -0,0 +1,29 @@
+package com.edutav.digitalwallet.infrastructure.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
